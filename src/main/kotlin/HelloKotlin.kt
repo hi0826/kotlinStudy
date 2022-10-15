57cb5d0fcd35f4b5a69f750fb78d02757fc98d52
hi0826 <kkkttt555@naver.com>
2022-10-15 15:43:44 +0900

    kotlin study chapter1

---
+++ src/main/kotlin/HelloKotlin.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("HelloKotlin!")
+    println("Hello~")
+    println("args[0] = ${args[0]}")
+    println(args[1])
+    println(args[2])
+    println(args[3])
+}
