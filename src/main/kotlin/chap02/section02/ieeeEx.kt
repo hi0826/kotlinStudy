7872fd8e12d17a1aea743226c3bd8b84a98ba715
hi0826 <kkkttt555@naver.com>
2022-10-21 18:35:38 +0900

    kotlin study 2-1

---
+++ src/main/kotlin/chap02/section02/ieeeEx.kt
@@ -0,0 +1,10 @@
+package chap02.section02
+
+fun main() {
+    var num: Double = 0.1
+
+    for (x in 0..999) {
+        num += 0.1
+    }
+    println("num: $num")
+}
---
+++ src/main/kotlin/dataType.kt.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val userName: String = "Kildong"
+    var userName2: String = "Kildong";
+    var count = 3
+    userName2 = "Dooly"
+    println("username: $userName, count: $count")
+    println("username2:$userName2")
+}
